836cb69ff65ea9c1d82b96b3b76c552339b3be70
1somakodi <info.somakodi@gmail.com>
2025-12-09 23:10:27 +0300

    Fix Next.js Netlify config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? '/NextJS-Tailwind-Course-Landing-Page/' : '',
-  basePath: isProd ? '/NextJS-Tailwind-Course-Landing-Page' : '',
+  reactStrictMode: true,
+  output: "standalone",
+
   images: {
     unoptimized: true,
   },
 
-  // ✅ Allow production build even if TypeScript errors exist
   typescript: {
     ignoreBuildErrors: true,
   },
 
-  // ✅ Allow production build even if ESLint errors exist
   eslint: {
     ignoreDuringBuilds: true,
   },
